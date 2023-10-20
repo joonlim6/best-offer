09363ac9eb5106658e29d0ac96bda6fb719f8d4d
joonlim6 <limjoon629@gmail.com>
2023-10-20 11:06:17 +0900

    first commit

---
+++ src/main/java/com/joonseok/bestoffer/BestofferApplication.java
@@ -0,0 +1,13 @@
+package com.joonseok.bestoffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestofferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestofferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joonseok/bestoffer/BestofferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joonseok.bestoffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestofferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
